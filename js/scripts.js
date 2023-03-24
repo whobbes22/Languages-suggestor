ba7db04c53597f5e7a0e0ff4d644a64ee704865e
Joe W <whobbes22@gmail.com>
2023-03-24 15:39:47 -0700

    add a question about 3's

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ function findScore(){
   let score = parseInt(document.querySelector("input[name='chal']:checked").value);
   score += parseInt(document.querySelector("input[name='corr']:checked").value);
   score += parseInt(document.querySelector("input[name='past']:checked").value);
+  score += parseInt(document.querySelector("input[name='thre']:checked").value);
   score += parseInt(document.querySelector("select#time").value);
   score += parseInt(document.querySelector("select#lett").value);
   return score;
