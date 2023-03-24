248a7f9b45744d35e6aceea3e18df4ae6d3eac37
Joe W <whobbes22@gmail.com>
2023-03-24 12:01:49 -0700

    WIP: Trying to get links to update to correct resource with corresponding languages

--- js/scripts.js
+++ js/scripts.js
@@ -25,16 +25,31 @@ function randomizedLanguage(event){
   revealAnswer(score);
 }
 function revealAnswer(score){
-  if(score >= 4){
-    //learn python  https://en.wikipedia.org/wiki/Python_(programming_language)
+  document.querySelector("div#results").removeAttribute("class");
+  if(score > 5){
+    //learn SQL    
+    document.querySelector("span#showLanguage").innerText ="SQL";
+    document.querySelector("spawn#showLink").href="https://en.wikipedia.org/wiki/SQL";
+  } else if(score >= 3){
+    //learn python  
+    document.querySelector("span#showLanguage").innerText ="Python";
+    document.querySelector("spawn#showLink").href="https://en.wikipedia.org/wiki/Python_(programming_language)";
   } else if(score > 0){
-    // learn js     https://en.wikipedia.org/wiki/JavaScript
+    // learn js     
+    document.querySelector("span#showLanguage").innerText ="JavaScript";
+    document.querySelector("spawn#showLink").href="https://en.wikipedia.org/wiki/JavaScript";
   } else if(score ===0){
-    // learn R      https://en.wikipedia.org/wiki/R_(programming_language)
+    // learn R      
+    document.querySelector("span#showLanguage").innerText ="R";
+    document.querySelector("spawn#showLink").href="https://en.wikipedia.org/wiki/R_(programming_language)";
   } else if(score >= -3){
-    //learn C       https://en.wikipedia.org/wiki/C_(programming_language)
+    //learn C       
+    document.querySelector("span#showLanguage").innerText ="C";
+    document.querySelector("spawn#showLink").href="https://en.wikipedia.org/wiki/C_(programming_language)";
   } else{
-    //learn C#      https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+    //learn C#      
+    document.querySelector("span#showLanguage").innerText ="C#";
+    document.querySelector("spawn#showLink").href="https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
   }
 }
 
